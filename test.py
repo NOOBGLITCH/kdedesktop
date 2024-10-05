1e6f6343dae5727ce43b28c8a3c6b170912530e4
The Disala <chamoddisala41@gmail.com>
2024-10-05 03:01:02 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import os
 import subprocess
 
 subprocess.run(['pip', 'install', '-q', 'colab-xterm'])
+
+CRD_SSH_Code = input("Google CRD SSH Code: ")
+username = "user"  # Change username if needed
+password = "root"  # Change password if needed
+Pin = 123456       # Set default PIN or modify it
+
+# Write the inputs to environment variables
+%env CRD_SSH_Code=$CRD_SSH_Code
+%env username=$username
+%env password=$password
+%env Pin=$Pin
