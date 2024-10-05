024ceb484555fcf76bc3fdc8e8505b3f80e69d54
The Disala <chamoddisala41@gmail.com>
2024-10-05 03:05:01 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import subprocess
 
 subprocess.run(['pip', 'install', '-q', 'colab-xterm'])
 
+# Python code to capture user input
 CRD_SSH_Code = input("Google CRD SSH Code: ")
-username = "user"  # Change username if needed
-password = "root"  # Change password if needed
-Pin = 123456       # Set default PIN or modify it
+username = "user"  # Default username, can be modified
+password = "root"  # Default password, can be modified
+Pin = 123456       # Default PIN
 
-# Write the inputs to environment variables
-%env CRD_SSH_Code=$CRD_SSH_Code
-%env username=$username
-%env password=$password
-%env Pin=$Pin
+# Save the inputs to a temporary file
+with open("user_input.txt", "w") as f:
+    f.write(f"CRD_SSH_Code={CRD_SSH_Code}\n")
+    f.write(f"username={username}\n")
+    f.write(f"password={password}\n")
+    f.write(f"Pin={Pin}\n")
